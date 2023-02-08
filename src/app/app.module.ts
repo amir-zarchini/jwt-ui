0555e91f5f1f91936f84e0712c83b3c47d9701a1
amir-zarchinie <amzarchini@gmail.com>
2023-02-08 09:24:38 +0330

    add component's( home, admin, user, login, forbidden, header) to project closes #2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
+import { HeaderComponent } from './header/header.component';
+import { ForbiddenComponent } from './forbidden/forbidden.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    UserComponent,
+    AdminComponent,
+    HeaderComponent,
+    ForbiddenComponent
   ],
   imports: [
     BrowserModule
