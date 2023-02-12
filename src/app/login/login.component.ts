78a2cfd5d4621e6910066cab4899e676874c230a
amir-zarchinie <amzarchini@gmail.com>
2023-02-12 08:26:19 +0330

    add user-auth service redirect authorized user in related page closes #8

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from '../service/user.service';
+import {UserAuthService} from "../service/user-auth.service";
 
 @Component({
   selector: 'app-login',
@@ -9,17 +10,24 @@ import { UserService } from '../service/user.service';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor(
-    private userService: UserService,
-  ) {}
+  constructor(private userService: UserService,
+              private userAuthService: UserAuthService,
+              private router: Router) {}
 
   ngOnInit(): void {}
 
   login(loginForm: NgForm) {
-    console.log(loginForm.form.value)
-    this.userService.login(loginForm.form.value).subscribe(
+    this.userService.login(loginForm.value).subscribe(
       (response: any) => {
-        console.log('response: ',response)
+        this.userAuthService.setRoles(response.user.role);
+        this.userAuthService.setToken(response.jwtToken);
+
+        const role = response.user.role[0].roleName;
+        if (role === 'Admin') {
+          this.router.navigate(['/admin']);
+        } else {
+          this.router.navigate(['/user']);
+        }
       },
       (error) => {
         console.log(error);
